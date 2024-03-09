32480bfeb20fe97d9d17babf58c88599d94198f1
chidang <dangtanminhchi@gmail.com>
2024-03-09 17:50:02 +0700

    Init package

---
+++ src/GeneratorBuilderServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Techamz\GeneratorBuilder;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeneratorBuilderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
